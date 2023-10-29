56a688ddefb017d90e25bd3ca4419d35170d553e
CHoneyfox <alex.fuchs970@gmail.com>
2023-10-29 19:37:05 +0100

    did a lil something with the game

--- pizzaway/game/static/src/main.cpp
+++ pizzaway/game/static/src/main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include <raylib.h>
 
+#define WIDTH 500
+#define HEIGHT 500
+
+
 int main()
 {
-    InitWindow(500, 500, "Title");
+    InitWindow(WIDTH, HEIGHT, "Title");
+
+    Vector2 position;
+    float radius = 3;
+
     while (!WindowShouldClose())
     {
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+            radius *= 1.05;
+        position = GetMousePosition();
+
         BeginDrawing();
             ClearBackground(RAYWHITE);
+            DrawCircleV(position, radius, RED);
         EndDrawing();
     }
 

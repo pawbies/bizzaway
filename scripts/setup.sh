88fc8e33b5d3e65fd9a471ee8184c0b927e6bb2b
CHoneyfox <alex.fuchs970@gmail.com>
2023-11-09 18:03:43 +0100

    replaced the makefile with bash scripts

---
+++ scripts/setup.sh
@@ -0,0 +1,9 @@
+script_dir="$(dirname "$(readlink -f "$0")")"
+cd "$script_dir/.."
+
+pip install django Pillow pytz
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init
+python manage.py makemigrations
+python manage.py migrate
+python manage.py test
